d2f5803a0318c934b5c6c23a534fd7a5283b8f6c
SandraClodion <clodionsandra@gmail.com>
2020-01-31 11:24:29 +0100

    Update README.py

--- README.py
+++ README.py
@@ -15,6 +15,7 @@ RED = (255, 0, 0)
 clock = pygame.time.Clock()
 
 name = input("Enter the name of your character : ")
+
 screen = pygame.display.set_mode((1200, 600))
 
 t=0
@@ -28,39 +29,22 @@ while running:
 
   for event in pygame.event.get(KEYDOWN):
     if event.key == K_q:
+      sys.exit(0)
       # on quitte le programme lors d'un appui sur Q
 
-"""
+
 class Monstre:
     def __init__(self,force,vie=True):
-        self.force=force
-        self.vie=vie
+        self.force
     
-    def deplacement(self):
-        #suivre le perso, en tenant compte des murs etc
+    def deplacement()
+    def vaincu
+        self.vie
+        self.donne_force
+        self.donne_vie
+    def avaincu
+        self.dommages=
 
-
-    def estvaincu(self,autre):
-        self.vie=False
-        self.donne_force(autre)
-        self.donne_vie(autre)
-
-    def avaincu(self,autre):
-        #self.dommages=
-        autre.vie=False
-        autre.donne_force(self)
-        autre.donne_vie(self)
-    
-    def donne_force(self,autre):
-        autre.force=self.force
-        self.force=0
-    
-    def donne_vie(self,autre):
-        autre.vie=self.vie
-        self.vie=0
-
-    #def dommages(self,autre):
-        
 
 class Chevalier(Monstre):
     
@@ -71,17 +55,14 @@ class Objet:
         self.localisation = (random.randint(len(piece.largeur), random.randint(len(piece.longueur)))
 
 
-
 class Potion(Objet):
     def __init__(self, piece):
         self.donne = 1 # point de vie
         self.localisation = Objet.localisation
-
 class Nourriture(Objet):
     def __init__(self,piece):
         self.donne = 1 # point de force 
         self.localisation = Objet.localisation
-
 
 class Argent(Objet):
     def __init__(self):
