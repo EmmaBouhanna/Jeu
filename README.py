4a65498d3958e1d71a46fb4ff0d6014fb7c9e8be
Didou <emma.bou_hanna@mines-paristech.fr>
2020-01-31 17:18:12 +0100

    Update README.py

--- README.py
+++ README.py
@@ -17,7 +17,7 @@ for event in pygame.event.get():
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREY = (150, 150, 150)
-RED = (255, 0, 0)
+RED = (255, 0, 0) 
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
