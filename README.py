923c672f3187b6d39e71fe071eb96bcfe0ee024c
SandraClodion <clodionsandra@gmail.com>
2020-01-31 15:20:44 +0100

    Update README.py

--- README.py
+++ README.py
@@ -5,6 +5,7 @@ import pygame
 from pygame.locals import *
 import random
 import numpy as np
+from classes import *
 
 pygame.init()
 
@@ -18,7 +19,7 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREY = (150, 150, 150)
 RED = (255, 0, 0)
-GREEN = (0, 255, 0)
+GREEN = (0, 255, 0) 
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 CYAN = (158, 255, 238)
@@ -32,6 +33,8 @@ grid = np.zeros((60,120))#salle1
 for i in range (3,23):
     grid[2,i]=1
     grid[5,i]=1
+    grid[3,i] = 4
+    grid[4,i]= 4
 grid[3,3]=1
 grid[4,3]=1
 grid[4,22]=1
@@ -43,6 +46,9 @@ for i in range(28,70): #salle2
 for j in range (15,31):
     grid[j,28] = 1
     grid[j,69] = 1
+for a in range(29,69):
+    for b in range(16,30):
+        grid[b,a]=4
 grid[15,33]=2
 grid[15,68]=2
 grid[30,55]=2
@@ -58,6 +64,9 @@ for i in range(65, 91):
 for j in range(1,14):
     grid[j,65]=1
     grid[j,90]=1
+for a in range(66,90):
+    for b in range(2,13):
+        grid[b,a]=4
 grid[13,68]=2
 for k in range(14,15):
     grid[k,68]=3
@@ -68,6 +77,9 @@ for j in range(38, 54):
 for i in range(40,66):
     grid[38,i]=1
     grid[53,i]=1
+for a in range(41,65):
+    for b in range(39,53):
+        grid[b,a]=4
 grid[38,55]=2
 grid[40,65]=2
 for k in range(31,38):
@@ -81,6 +93,9 @@ for j in range(35,46):
     grid[j,110]=1
 grid[40,80] = 2
 grid[35,83] = 2
+for a in range(81,110):
+    for b in range(36,45):
+        grid[b,a]=4
 for k in range(66,80):
     grid[40,k]=3
 for k in range(34,35):
@@ -107,6 +122,8 @@ def draw_level(Grid):
                 draw_cell(pos, RED)
             elif grid[l,c] == 3 : #couloir
                 draw_cell(pos, GREY)
+            elif grid[l,c] == 4 : #intérieur des salles
+                draw_cell(pos, CYAN)
 
 clock = pygame.time.Clock()
 name = input("Enter the name of your character : ")
@@ -139,11 +156,15 @@ while running:
         elif event.key==K_RIGHT:
             dx,dy=1,0
         # on quitte le programme lors d'un appui sur Q
-    player = Personnage()
     if t>200:
         t=0
         draw_level(grid)
+        perso = pygame.image.load().convert()
+        screen.blit(perso, (4,5))
         pygame.display.update()
+        
+
+ 
 
 
 
