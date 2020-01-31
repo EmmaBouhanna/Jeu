acee716754ad297c2c47bd6297f018cee3dd6660
SandraClodion <clodionsandra@gmail.com>
2020-01-31 10:23:12 +0100

    J'ai ajouté des couleurs

--- README.py
+++ README.py
@@ -1,1 +1,13 @@
 # Jeu
+
+import sys
+import pygame
+from pygame.locals import *
+import random
+
+pygame.init()
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+RED = (255, 0, 0)
