ed86c85f91061fd1aceddb13845939931e5d8736
myrtilledavid <54951743+myrtilledavid@users.noreply.github.com>
2020-01-31 11:11:59 +0100

    Update README.py

--- README.py
+++ README.py
@@ -14,16 +14,34 @@ RED = (255, 0, 0)
 
 class Monstre:
     def __init__(self,force,vie=True):
-        self.force
+        self.force=force
+        self.vie=vie
     
-    def deplacement()
-    def vaincu
-        self.vie
-        self.donne_force
-        self.donne_vie
-    def avaincu
-        self.dommages=
+    def deplacement(self):
+        #suivre le perso, en tenant compte des murs etc
 
+
+    def estvaincu(self,autre):
+        self.vie=False
+        self.donne_force(autre)
+        self.donne_vie(autre)
+
+    def avaincu(self,autre):
+        #self.dommages=
+        autre.vie=False
+        autre.donne_force(self)
+        autre.donne_vie(self)
+    
+    def donne_force(self,autre):
+        autre.force=self.force
+        self.force=0
+    
+    def donne_vie(self,autre):
+        autre.vie=self.vie
+        self.vie=0
+
+    #def dommages(self,autre):
+        
 
 class Chevalier(Monstre):
     
