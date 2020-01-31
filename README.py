27aa8e46eeb19d4c51d8fa40069b34d2f163f194
SophieRossi <sphd.rossi@gmail.com>
2020-01-31 11:47:50 +0100

    Update README.py

--- README.py
+++ README.py
@@ -88,4 +88,34 @@ class Armure(Objet):
 class Arme(Objet):
     def __init(self, piece)
         self.localisation = Objet.localisation
-        self.donne = random.randint(4, 6) """
+        self.donne = random.randint(4, 6) 
+
+
+
+class Personnage:
+    def __init__(self):
+        self.force = 10
+        self.pt_vie = 3
+        self.sac = []
+        self.armes = []
+        self.pos = [] 
+    
+    def __repr__(self):
+        print("@")
+    
+    def attack(self):
+          
+        
+    def move_me(self, dx, dy):
+        x = self.pos[0]+dx
+        y = self.pos[1]+dy
+        el = grid.get_el(x*grid.cell_size, (y)*grid.cell_size)
+        
+        if el == "." or el == "+" or el == "#":
+            self.pos[0] = x
+            self.pos[1] = y 
+            return True
+        # if el == "B" or el == "K":
+            
+        else:
+            return False
