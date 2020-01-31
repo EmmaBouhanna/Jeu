4870422ec8bef4eed809bc740c197a0c6744d6f7
Didou <emma.bou_hanna@mines-paristech.fr>
2020-01-31 10:38:52 +0100

    Update README.py

--- README.py
+++ README.py
@@ -11,3 +11,19 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 YELLOW = (255, 255, 0)
 RED = (255, 0, 0)
+
+class Monstre:
+    def __init__(self,force,vie=True):
+        self.force
+    
+    def deplacement()
+    def vaincu
+        self.vie
+        self.donne_force
+        self.donne_vie
+    def avaincu
+        self.dommages=
+
+
+class Chevalier(Monstre):
+    
